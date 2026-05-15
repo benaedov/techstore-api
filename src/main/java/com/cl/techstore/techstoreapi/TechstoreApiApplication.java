99bd4ca88fbc7f22eb886038ef66667884310b8c
beaedov <be.aedov@duocuc.cl>
2026-05-15 14:00:32 -0400

    feat: estructura inicial del proyecto

---
+++ src/main/java/com/cl/techstore/techstoreapi/TechstoreApiApplication.java
@@ -0,0 +1,13 @@
+package com.cl.techstore.techstoreapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechstoreApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechstoreApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cl/techstore/techstoreapi/TechstoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cl.techstore.techstoreapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechstoreApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
